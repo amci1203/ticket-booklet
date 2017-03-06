13692df68243ffd79fbbc7f8e3986e5db46f0b5f
amci1203 <allen.mcintoshii@gmail.com>
2017-03-06 01:17:51 -0500

    Injector getAll feature done

--- app/assets/js/modules/Injector.js
+++ app/assets/js/modules/Injector.js
@@ -23,7 +23,8 @@ export default function Injector (callback) {
                 else if (this.hasAttribute('data-view-all')) {
                     const val    = $(this).attr('data-view-all'),
                           folder = val.slice(0, val.indexOf('[')).trim(),
-                          views  = JSON.parse(val.string(val.indexOf('[')));
+                          views  = JSON.parse(val.substring(val.indexOf('[')));
+                    console.log({val, folder, views})
                     getAll.call(this, folder, ...views);
                 }
             })
@@ -31,10 +32,13 @@ export default function Injector (callback) {
         else return false;
     }
     
-    function fetchNestedFiles (partial) {
-        const parentFolderNames = partial.attr('data-view').split('/').filter(str => str != '..').slice(0, -1),
-              numParentFolders  = parentFolderNames.length,
-              nestedInclusions  = partial.find('._partial');
+    function fetchNestedFiles (partial, specPath) {
+        const
+            pathToUseAsParent = specPath ? specPath : partial.attr('data-view'),
+            parentFolderNames = pathToUseAsParent.split('/').filter(str => str != '..').slice(0, -1),
+            numParentFolders  = parentFolderNames.length,
+            nestedInclusions  = partial.find('._partial');
+        
         if (nestedInclusions.length > 0) {
             nestedInclusions.each(function () {
                 const 
@@ -49,7 +53,6 @@ export default function Injector (callback) {
                     view              = findView(realPath.join('/'));
                 
                 const vars = { parentFolderNames, numParentFolders, nestedPath, fixedNestedPath, dirsUp, numParentDirs, dirInboundOfViews, parentPath, view}
-                console.log(vars);
                 
                 get.call(this, view);
             })
@@ -76,17 +79,20 @@ export default function Injector (callback) {
         const views    = [..._views],
               numViews = views.length;
         let html = '';
-        views.forEach(view => {
-            const path = `${folder}/${view}`;
-            $.get(path)
+        views.forEach((view, index) => {
+            const path     = `${folder}/${view}`,
+                  realPath = findView(path);
+            $.get(realPath)
                 .done(data => html += data)
                 .fail(err => {})
                 .always(() => {
-                    setTimeout( fetchNestedFiles.bind(this, $(this)), wait )
+                    setTimeout( fetchNestedFiles.bind(this, $(this), path), wait )
+                    if (index + 1 == numViews) {
+                        $(this).html(html)
+                        incrementInjectionsDone();
+                    }
                 });
         })
-        $(this).html(data)
-        setTimeout(incrementInjectionsDone, wait);
     }
     
     return (fetchFiles())
