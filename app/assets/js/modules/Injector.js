a97cc9bd4da9e2a778b9af661a715245261ff777
amci1203 <allen.mcintoshii@gmail.com>
2017-03-06 16:19:47 -0500

    Working on handling different Inject cases

--- app/assets/js/modules/Injector.js
+++ app/assets/js/modules/Injector.js
@@ -2,47 +2,56 @@ import $ from 'jquery';
 
 export default function Injector (callback) {
     const 
+        pageViews     = $('._page'),
         partialViews  = $('._partial'),
-        viewsRoot     = 'views/',
-        numViews      = partialViews.length,
+        partialsRoot  = 'views/partials/',
+        pagesRoot     = 'views/',
+        numViews      = partialViews.length + pageViews.length,
         wait          = 150;
     let 
         viewsInjected = 0;
     
-    function findView (string) {
-        return `${viewsRoot}${string}.html`;
+    function findPartial (string) {
+        return `${partialsRoot}${string}.html`;
+    }
+    function findPage (string) {
+        return `${pagesRoot}${string}.html`;
     }
     
     function fetchFiles () {
+        function iterate () {
+            if (this.hasAttribute('data-view')) {
+                const path = $(this).attr('data-view'),
+                      view = $(this).hasClass('_partial') ? findPartial(path) : findPage(path);
+                get.call(this, view);
+            }
+            else if (this.hasAttribute('data-view-all')) {
+                const val    = $(this).attr('data-view-all'),
+                      folder = val.slice(0, val.indexOf('[')).trim(),
+                      views  = JSON.parse(val.substring(val.indexOf('[')));
+                console.log({val, folder, views})
+                getAll.call(this, folder, ...views);
+            }
+        }
         if (numViews > 0) {
-            partialViews.each(function () {
-                if (this.hasAttribute('data-view')) {
-                    const view = findView( $(this).attr('data-view') );
-                    get.call(this, view);
-                }
-                else if (this.hasAttribute('data-view-all')) {
-                    const val    = $(this).attr('data-view-all'),
-                          folder = val.slice(0, val.indexOf('[')).trim(),
-                          views  = JSON.parse(val.substring(val.indexOf('[')));
-                    console.log({val, folder, views})
-                    getAll.call(this, folder, ...views);
-                }
-            })
+            partialViews.each(iterate);
+            pageViews.each(iterate);
         }
         else return false;
     }
     
-    function fetchNestedFiles (partial, specPath) {
+    function fetchNestedFiles (specPath) {
         const
-            pathToUseAsParent = specPath ? specPath : partial.attr('data-view'),
+            pathToUseAsParent = specPath ? specPath : $(this).attr('data-view'),
             parentFolderNames = pathToUseAsParent.split('/').filter(str => str != '..').slice(0, -1),
             numParentFolders  = parentFolderNames.length,
-            nestedInclusions  = partial.find('._partial');
+            nestedInclusions  = $(this).find('._partial, ._page');
         
         if (nestedInclusions.length > 0) {
             nestedInclusions.each(function () {
-                const 
-                    nestedPath      = $(this).attr('data-view').split('/'),
+                const
+                    isPartial       = $(this).hasClass('_partial'),
+                    nestedPath      = pathToUseAsParent.split('/'),
                     fixedNestedPath = nestedPath.filter(str => str != '..'),
                     dirsUp          = nestedPath.filter(str => str == '..').length,
 
@@ -50,9 +59,10 @@ export default function Injector (callback) {
                     dirInboundOfViews = numParentDirs >= 0,
                     parentPath        = dirInboundOfViews ? parentFolderNames.slice(0, numParentDirs) : [],
                     realPath          = nestedPath[0] != '' ? [...parentPath, ...fixedNestedPath] : nestedPath,
-                    view              = findView(realPath.join('/'));
+                    view              = isPartial ? findPartial(realPath.join('/')) : findPage(realPath.join('/'));
                 
-                const vars = { parentFolderNames, numParentFolders, nestedPath, fixedNestedPath, dirsUp, numParentDirs, dirInboundOfViews, parentPath, view}
+                const vars = { isPartial, parentFolderNames, numParentFolders, nestedPath, fixedNestedPath, dirsUp, numParentDirs, dirInboundOfViews, parentPath, view}
+                console.log(vars)
                 
                 get.call(this, view);
             })
@@ -70,7 +80,7 @@ export default function Injector (callback) {
             .done(data => $(this).html(data))
             .fail(err => {})
             .always(() => {
-                setTimeout( fetchNestedFiles.bind(this, $(this)), wait );
+                setTimeout( fetchNestedFiles.bind(this), wait );
                 incrementInjectionsDone();
             })
     }
@@ -86,7 +96,7 @@ export default function Injector (callback) {
                 .done(data => html += data)
                 .fail(err => {})
                 .always(() => {
-                    setTimeout( fetchNestedFiles.bind(this, $(this), path), wait )
+                    setTimeout( fetchNestedFiles.bind(this, path), wait )
                     if (index + 1 == numViews) {
                         $(this).html(html)
                         incrementInjectionsDone();
