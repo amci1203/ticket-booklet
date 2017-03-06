0a0a2e29d50de7f2d1595f88d0eb04d819a06a4e
amci1203 <allen.mcintoshii@gmail.com>
2017-03-06 00:37:29 -0500

    Added getAll method to Injector

--- app/assets/js/modules/Injector.js
+++ app/assets/js/modules/Injector.js
@@ -1,17 +1,29 @@
 import $ from 'jquery';
 
 export default function Inject (callback) {
-    const partialViews  = $('._partial'),
-          viewsRoot     = 'views/',
-          numViews      = partialViews.length,
-          findView      = string => `${viewsRoot}${string}.html`;
-    let viewsInjected = 0;
+    const 
+        partialViews  = $('._partial'),
+        viewsRoot     = 'views/',
+        numViews      = partialViews.length,
+        wait          = 150;
+    let 
+        viewsInjected = 0;
+    
+    function findView (string) { return `${viewsRoot}${string}.html` }
     
     function fetchFiles () {
         if (numViews > 0) {
             partialViews.each(function () {
-                const view = findView( $(this).attr('data-view') );
-                get.call(this, view);
+                if (this.hasAttribute('data-view')) {
+                    const view = findView( $(this).attr('data-view') );
+                    get.call(this, view);
+                }
+                else if (this.hasAttribute('data-view-all')) {
+                    const val    = $(this).attr('data-view-all'),
+                          folder = val.slice(0, val.indexOf('[')).trim(),
+                          views  = JSON.parse(val.string(val.indexOf('[')));
+                    getAll.call(this, folder, ...views);
+                }
             })
         }
         else return false;
@@ -40,17 +52,38 @@ export default function Inject (callback) {
         else return false;
     }
     
+    function incrementViewsInjected () {
+        viewsInjected++;
+        if (viewsInjected == numViews) {
+            setTimeout(callback, wait)
+        };
+    }
+    
     function get (path) {
         $.get(path)
-        .done(data => $(this).html(data))
-        .fail(err => {})
-        .always(() => {
-            setTimeout( fetchNestedFiles.bind(this, $(this)), 200 )
-            viewsInjected++;
-            if (viewsInjected == numViews) {
-                setTimeout(callback, 200)
-            };
+            .done(data => $(this).html(data))
+            .fail(err => {})
+            .always(() => {
+                setTimeout( fetchNestedFiles.bind(this, $(this)), wait );
+                incrementViewsInjected();
+            })
+    }
+    
+    function getAll (folder, ..._views) {
+        const views    = [..._views],
+              numViews = views.length;
+        let html = '';
+        views.forEach(view => {
+            const path = `${folder}/${view}`;
+            $.get(path)
+                .done(data => html += data)
+                .fail(err => {})
+                .always(() => {
+                    setTimeout( fetchNestedFiles.bind(this, $(this)), wait )
+                });
         })
+        $(this).html(data)
+        setTimeout(incrementViewsInjected, wait);
     }
     
     return (fetchFiles())
